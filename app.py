849ee7310698afe909ff23c428eab5ca5f77e1ad
navewow <nave.wow@gmail.com>
2017-01-11 12:35:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def makeWebhookResult(data):
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
